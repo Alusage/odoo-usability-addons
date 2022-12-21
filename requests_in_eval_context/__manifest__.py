e99d56dacaf6b7bf83cf0889d500b362167fef4b
Nicolas JEUDY <nicolas.jeudy@myceliandre.fr>
2022-12-21 16:28:05 +0100

    [ADD] new module to update eval context (request, base64..)

---
+++ requests_in_eval_context/__manifest__.py
@@ -0,0 +1,30 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    'name': 'Requests in eval_context',
+    'version': '14.0.1.0.0',
+    'summary': """
+        This module enable using requests in python code (base_automation, ir.actions)
+    """,
+    'description': """
+    """,
+    'author': 'Nicolas JEUDY (Alusage)',
+    'website': 'https://nicolas.alusage.fr',
+    'license': 'AGPL-3',
+    'category': 'Technical',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'demo': [],
+    'auto_install': False,
+    'external_dependencies': [],
+    'application': False,
+    'css': [],
+    'images': [],
+    'installable': True,
+    'maintainer': 'Nicolas JEUDY',
+    'pre_init_hook': '',
+    'post_init_hook': '',
+    'uninstall_hook': '',
+}
---
+++ requests_in_eval_context/models/ir_cron.py
@@ -0,0 +1,29 @@
+import requests
+import logging
+import base64
+from odoo import api, fields, models, _
+
+_logger = logging.getLogger(__name__)
+
+class IrCron(models.Model):
+    _inherit = 'ir.cron'
+
+
+    @api.model
+    def _get_eval_context(self, action=None):
+        context = super(IrCron, self)._get_eval_context().copy()
+        def request_get(*args, **kwargs):
+            return requests.get(*args, **kwargs)
+        def request_post(*args, **kwargs):
+            return requests.post(*args, **kwargs)
+
+        context.update(
+            {
+                "encodestring": base64.encodestring,
+                "request_post": request_post,
+                "request_get": request_get,
+                "b64encode": base64.b64encode,
+                "b64decode": base64.b64decode,
+            }
+        )
+        return context
