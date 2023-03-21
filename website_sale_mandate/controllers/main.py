3e62fc323d49d162c597de57b78d91d8ae65a0aa
Nicolas JEUDY <nicolas.jeudy@myceliandre.fr>
2023-03-21 13:48:56 +0100

    [NEW] add controler and form for SEPA mandate in extra shop step

--- website_sale_mandate/controllers/main.py
+++ website_sale_mandate/controllers/main.py
@@ -7,6 +7,7 @@ from odoo.http import request
 from odoo.addons.base.models.ir_qweb_fields import nl2br
 from odoo.exceptions import ValidationError
 from odoo.addons.website_sale.controllers.main import WebsiteSaleForm
+from odoo.addons.base_iban.models.res_partner_bank import validate_iban
 
 _logger = logging.getLogger(__name__)
 
@@ -20,6 +21,8 @@ class WebsiteSaleFormCustom(WebsiteSaleForm):
         website=True,
     )
     def website_form_saleorder(self, **kwargs):
+        error = {}
+        error_message = []
         custom_data = {}
         # TODO: properly inherit with super()
         # response = super(WebsiteSaleFormCustom, self).website_form_saleorder(**post)
@@ -39,7 +42,25 @@ class WebsiteSaleFormCustom(WebsiteSaleForm):
             order.write(data["record"])
 
         if custom_data:
-            if custom_data['iban']:
+            if custom_data.get('iban', False):
+                try:
+                    validate_iban(custom_data['iban'])
+                except ValidationError as e:
+                    values = kwargs
+                    error_message.append(e.args[0])
+                    error["iban"] = 'error'
+                    render_values = {
+                        'website_sale_order': order,
+                        'checkout': values,
+                        'error': error,
+                        'callback': kwargs.get('callback'),
+                        'only_services': order and order.only_services,
+                        "error_message": error_message
+                    }
+                    _logger.debug('iban error: %s' % custom_data['iban'])
+                    #return request.render("website_sale.extra_info", render_values)
+                    # return json.dumps(render_values)
+                    #return request.redirect("/shop/extra_info", render_values)
                 custom_data['authorize_iban_direct_debit'] = True
             order.write(custom_data)
 
--- website_sale_mandate/models/sale_order.py
+++ website_sale_mandate/models/sale_order.py
@@ -1,9 +1,43 @@
+from datetime import datetime
 from odoo import api, fields, models, SUPERUSER_ID, _
 
 class SaleOrder(models.Model):
     _inherit = "sale.order"
 
+    def _show_mandate_action(self):
+        for order in self:
+            order.show_mandate_action = False
+            if order.iban and order.iban != order.partner_id.valid_mandate_id.partner_bank_id.acc_number:
+                order.show_mandate_action = True
+         
+
     authorize_iban_direct_debit = fields.Boolean('Active Direct Debit (SEPA)')
+    show_mandate_action = fields.Boolean("Show Mandate action", compute='_show_mandate_action')
     iban = fields.Char("IBAN")
     iban_contact = fields.Char('Iban Contact')
     
+    @api.multi
+    def action_create_partner_mandate(self):
+        
+        mandate_obj = self.env['account.banking.mandate']
+        bank_obj = self.env['res.partner.bank']
+
+        for order in self:
+            if order.iban:
+                new_bank = bank_obj.create({
+                    'acc_number': order.iban,
+                    'type': 'iban',
+                    'partner_id': order.partner_id.id,
+                })
+
+                mandate_obj.create({
+                    'format': 'sepa',
+                    'type': 'recurrent',
+                    'partner_bank_id': new_bank.id,
+                    'partner_id': order.partner_id.id,
+                    'structure': 'CORE',
+                    'recurent_sequence_type': 'first',
+                    'signature_date': datetime.today().strftime('%Y-%m-%d'),
+                })
+
+        return True
